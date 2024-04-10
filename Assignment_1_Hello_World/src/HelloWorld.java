d35625eace8e35679e0e7d9fc91d62f2558c97c0
Timothy Kwock <135040767+NovicePaso@users.noreply.github.com>
2024-04-09 17:18:50 -0700

    Upload CS1A Assignment 1 - Hello World!
    Test run of workflow uploading programs to github repo

---
+++ Assignment_1_Hello_World/src/HelloWorld.java
@@ -0,0 +1,56 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      System.out.println("My name is Timothy Kwock");
+      System.out.println("My student ID is 20578786");
+      System.out.println("");
+      System.out.println(
+              "Syllabus detail #1: Office hours are Thursdays 2-3pm via Zoom.");
+      System.out.println("");
+      System.out.println(
+              "Syllabus detail #2: Late assignments will be penalized 10% per "
+                      + "day (2pm cutoff),");
+      System.out.println("3 days.");
+      System.out.println("");
+      System.out.println(
+              "Syllabus detail #3: Asking a good question should include "
+                      + "specifically what the problem is, what you've tried, and where "
+                      + "you are having trouble.");
+      System.out.println("");
+      System.out.println(
+              "Specific lab detail #1: Don't use extra blanks, tabs or spaces "
+                      + "per Rule 5 of ");
+      System.out.println("Style Rules.  They should be concise while "
+              + "clearly identifying groups of code.");
+      System.out.println("");
+      System.out.println(
+              "Specific lab detail #2: Include a space around all operators per "
+                      + "Rule 6 of Style Rules. This will make sure the codes more "
+                      + "legible and spaced out.");
+   }
+}
+
+/*--------------------------------- Output ------------------------------------
+
+Hello World!
+My name is Timothy Kwock
+My student ID is 20578786
+
+Syllabus detail #1: Office hours are Thursdays 2-3pm via Zoom.
+
+Syllabus detail #2: Late assignments will be penalized 10% per day (2pm cutoff),
+3 days.
+
+Syllabus detail #3: Asking a good question should include specifically what the 
+problem is, what you've tried, and where you are having trouble.
+
+Specific lab detail #1: Don't use extra blanks, tabs or spaces per Rule 5 of 
+Style Rules.  They should be concise while clearly identifying groups of code.
+
+Specific lab detail #2: Include a space around all operators per Rule 6 of Style 
+Rules. This will make sure the codes more legible and spaced out.
+
+
+-----------------------------------------------------------------------------*/
